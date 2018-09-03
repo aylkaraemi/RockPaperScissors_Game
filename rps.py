7f35a9cad300d06109a2665d4af8e8464eab564e
Kayla Carpenter <aylkaraemi@gmail.com>
2018-09-03 14:32:05 -0600

    Add new move sets

--- rps.py
+++ rps.py
@@ -3,7 +3,12 @@
 """This program plays a game of Rock, Paper, Scissors between two Players,
 and reports both Player's scores each round."""
 
-moves = ['rock', 'paper', 'scissors']
+std_moves = ['rock', 'paper', 'scissors']
+
+rpsls = ['rock', 'paper', 'scissors', 'lizard', 'Spock']
+
+pokemon = ['Charmander', 'Bulbasaur', 'Squirtle']
+
 
 """The Player class is the parent class for all of the Players
 in this game"""
